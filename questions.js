684bc96743079a2688378831dc1f7e6bff308c98
petricstefan <petric.stefan@outlook.com>
2026-07-17 16:41:44 +0200

    Fix correct answers for hackathon questions

--- questions.js
+++ questions.js
@@ -14,37 +14,37 @@ const QUIZ_CONFIG = {
     {
       question: "Q1. Who started the Microsoft Global Hackathon?",
       answers: ["Bill Gates ", "Steve Ballmer ", "Satya Nadella", "Kevin Scott "],
-      correct: 3,
+      correct: 2,
       time: 20
     },
     {
       question: "Q2. In which year was the first Microsoft Global Hackathon held? ",
       answers: ["2010 ", "2012 ", "2014", "2016 "],
-      correct: 3,
+      correct: 2,
       time: 15
     },
     {
       question: "Q3. How long does Microsoft Global Hackathon typically run? ",
       answers: ["1 day ", "3 days ", "1 week", "1 month "],
-      correct: 3,
+      correct: 2,
       time: 20
     },
     {
       question: "Q4. Which organization runs the Microsoft Global Hackathon? ",
       answers: ["CELA ", "Engineering ", "MCAPS ", "The Garage"],
-      correct: 4,
+      correct: 3,
       time: 15
     },
     {
       question: "Q5. What do you need before registering for Hackathon? ",
       answers: ["A complete project plan ", "A team of at least 3 people ", "Manager approval ", "None of the above"],
-      correct: 4,
+      correct: 3,
       time: 15
     },
      {
       question: "Q6. What is the best time to join Hackathon? ",
       answers: ["After you have a complete idea  ", "After you have a full team ", "During Hackathon Week  ", "Right now — even if you're still exploring ideas"],
-      correct: 4,
+      correct: 3,
       time: 15
     }
   ]
